5337ce829a8efc04f664cc1e834f1f4d3b0a20c6
Pankaj Kumar Tiwari <pankajkumar.tiwari@wynk.in>
2022-10-11 01:26:32 +0530

    mongodb and mongoose setup

--- express-server/app.js
+++ express-server/app.js
@@ -1,118 +1,27 @@
-const { fail } = require("assert");
 const express = require("express");
-const fs = require("fs");
 const morgan = require('morgan');
-const mongoose = require('mongoose')
-const connectDB = require('./server')
+
+const tourRouter = require('./routes/tourRoutes');
+// const userRouter = require('./routes/userRoutes');
 
 const app = express();
-connectDB()
+
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req,res,next)=>{
     console.log('Hellow working as a middleware');
     next();
 });
+app.use((req,res,next)=>{
+  req.requestTime = new Date().toISOString();
+    next();
+});
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+// app.use('/api/v1/users', userRouter);
 
-/* app.get('/', (req, res) => {
-    res.status(200)
-    .json({ message: "Welcome" });
-})
-
-app.get('/users', (req, res) => {
-    res.json({ message: "users" })
-})
-
-app.post('/users/:id', (req, res) => {
-    const id = req.params.id;
-    const body = req.body;
-    res.json({ body, id })
-}) */
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data.json`));
-const getAllTours = (req, res) => {
-    res.status(200).json({
-      status: "sucess",
-      results: tours.length,
-      data: {
-        tours,
-      },
-    });
-  }
-
-const getTour = (req, res) => {
-    console.log(req.params);
-    const id = req.params.id * 1;
-    const tour = tours.find((el) => el.id == id);
-  
- if (!tour) {
-      return res.status(404).json({
-        status: "fail",
-        message: "Invalid ID",
-      });
-    }
-  
-    res.status(200).json({
-      status: "sucess",
-    });
-  };
-const addTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-    tours.push(newTour);
-    fs.writeFile(
-      `${__dirname}/dev-data/data.json`,
-      JSON.stringify(tours),
-      (error) => {
-        res.status(201).json({
-          status: "success",
-          data: {
-            tour: newTour,
-          },
-        });
-      }
-    );
-  }; 
-const updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-      return res.status(404).json({
-        status: "fail",
-        message: "Invalid ID",
-      });
-    }
-    res.status(200).json({
-      status: "success",
-      data: {
-        tour: "<Updated tour here ...>",
-      },
-    });
-  }
-const deleteTour =  (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-      return res.status(404).json({
-        status: "fail",
-        message: "Invalid ID",
-      });
-    }
-    res.status(200).json({
-      status: "success",
-      data: {
-        tour: "<data deleted ...>",
-      },
-    });
-  };
-
-app
-.route("/api/v1/tours")
-.get(getAllTours)
-.post(addTour);
-
-app
-.route("/api/v1/tours/:id")
-.get(getTour)
-.patch(updateTour)
-.delete(deleteTour);
-
-const port = 5000;
-app.listen(port, () => console.log(`app runing on port ${port}`));
+module.exports = app;
--- express-server/server.js
+++ express-server/server.js
@@ -1,20 +1,20 @@
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
 const app = require("./app");
+// const  mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
 
-const DB = process.env.DATABASE_LOCAL;
-// console.log(app.get('env'));
-const connectDB = () => {
-  mongoose
-    .connect("mongodb://localhost:27017/my-db-test")
-    .then(() => console.log("database connected"))
-    .catch(() => {
-      console.log("server is not started something wrong");
-    });
-};
-// const port = process.env.PORT || 5000;
-// app.listen(port,()=>{
-//     console.log(`App is running on port ${port}`)
-// });
-module.exports = connectDB
+// const DB = process.env.DATABASE_LOCAL;
+// mongoose.connect(DB,{
+//         useNewUrlParser:true,
+//         useCreateIndex:true,
+//         useFindAndModify:false,
+//     })
+//     .then(() => console.log("database connected"))
+//     .catch(() => {
+//       console.log("server is not started something wrong");
+//     });
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log(`App is running on port ${port}`)
+});
